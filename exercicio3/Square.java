5403ef0f9c92e5dda76ad188c162862581c2028c
Mario Alex <alexxbarbosa662@gmail.com>
2026-04-01 10:29:56 -0300

    feat: Colocando exceção no Square

--- exercicio3/Square.java
+++ exercicio3/Square.java
@@ -1,14 +1,23 @@
 public class Square extends Rectangle
 {
-    public Square(Point A, Point B, Point C, Point D)
-    {
-        super(A,B,C,D);
-    }
+	public Square(Point A, Point B, Point C, Point D)
+	{
+		super(A,B,C,D);
+	}
 
-    @Override
-    public double getArea()
-    {
-        return Math.pow(getA().distanceTo(getB()), 2.0);
-    }
+	// Sobrescreve o método getArea da classe Rectangle
+	@Override
+	public double getArea()
+	{
+		double lado1 = getA().distanceTo(getB());
+		double lado2 = getB().distanceTo(getC());
+
+		if (lado1 != lado2)
+		{
+			throw new IllegalArgumentException("Os pontos não formam um quadrado");
+		}
+
+		// fórmula: lado * lado
+		return Math.pow(getA().distanceTo(getB()), 2.0);
+	}
 }
-
